df593bc9dae26bb1fface956c5568563b59ffcae
amirtanazzoh <amirtanazzoh@gmail.com>
2025-08-05 20:16:40 +0330

    change structre and install shadcn

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home ()
+{
+  return (
+    <main className="flex items-center justify-center h-screen bg-gray-100">
+      <div className="bg-white shadow-md p-6 rounded-lg">
+        <h1 className="text-3xl font-bold text-blue-600">Tailwind is working! 🎉</h1>
+      </div>
+    </main>
+  );
+}
